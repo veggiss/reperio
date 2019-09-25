0ebd5ec7b228cf0dc90010245b85c4c621da79e6
Vegard Egge Schau <vegaes15@uia.no>
2019-09-25 09:41:55 +0200

    Updated app pages

--- src/app/components/xp-bar/xp-bar.module.ts
+++ src/app/components/xp-bar/xp-bar.module.ts
@@ -11,17 +11,16 @@ import { XpBarComponent } from './xp-bar.component';
     IonicModule,
     CommonModule,
     NgCircleProgressModule.forRoot({
-      radius: 60,
-      space: -10,
-      outerStrokeWidth: 10,
+      radius: 25,
+      space: -5,
+      outerStrokeWidth: 5,
       outerStrokeColor: "#4882c2",
       innerStrokeColor: "#e7e8ea",
-      innerStrokeWidth: 10,
-      animateTitle: true,
-      animationDuration: 1000,
-      showUnits: true,
+      innerStrokeWidth: 5,
       showBackground: false,
-      startFromZero: false,
+      animateTitle: false,
+      showTitle: false,
+      showUnits: false,
       showSubtitle: false
     })
   ]
